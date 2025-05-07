e2cc5bfbc2b72133b62954716b65e7575ca4cf21
emretoka28 <emre.toka@studium.uni-hamburg.de>
2025-05-07 14:13:48 +0200

    Command for deleting ML task pipeline

---
+++ delete_ml_tasks.sh
@@ -0,0 +1,5 @@
+rm -r /data/dust/user/tokaemre/hhh4b2w/data/cf_store/analysis_hhh4b2w/cf.PrepareMLEvents
+rm -r /data/dust/user/tokaemre/hhh4b2w/data/cf_store/analysis_hhh4b2w/cf.MergeMLStats
+rm -r /data/dust/user/tokaemre/hhh4b2w/data/cf_store/analysis_hhh4b2w/cf.MergeMLEvents
+rm -r /data/dust/user/tokaemre/hhh4b2w/data/cf_store/analysis_hhh4b2w/cf.MLTraining
+rm -r /data/dust/user/tokaemre/hhh4b2w/data/cf_store/analysis_hhh4b2w/cf.MLEvaluation
